cc51a3ceed8e5141e16800898ec942a4138796d0
Fede Camara Halac <camarafede@gmail.com>
2017-06-23 11:45:14 -0400

    _fk takes a piitch velocity pair and also a fader input to make chaotic filteer coeffs to an impulse. it uses biquad in conjunction with some raw filters

--- lor~/lor~.c
+++ lor~/lor~.c
@@ -1,6 +1,5 @@
 #include "m_pd.h"
-#include <math.h>
-/* ---------------- lor~ - raw lor filter ----------------- */
+/* ---------------- lor~ ----------------- */
 
 typedef struct lorctl
 {
@@ -10,15 +9,11 @@ typedef struct lorctl
     t_sample c_y2;
     t_sample c_z1;
     t_sample c_z2;
-    t_sample c_d1;
-    t_sample c_d2;
     t_sample c_ia;
     t_sample c_ib;
     t_sample c_ic;
     t_sample c_ih;
-    t_sample min;
-    t_sample max;
-    //t_sample c_ff3;
+
 } t_lorctl;
 
 typedef struct siglor
@@ -40,18 +35,18 @@ static void *siglor_new(t_symbol *s, int argc, t_atom *argv)
     x->x_ctl = &x->x_cspace;
     x->x_cspace.c_x1 = x->x_cspace.c_x2 = 0;
     siglor_list(x, s, argc, argv);
-    x->x_cspace.min = 0;
-    x->x_cspace.max = 10000000000;
     x->x_f = 0;
     return (x);
 }
 
 static t_int *siglor_perform(t_int *w)
 {
-    t_sample *in = (t_sample *)(w[1]);
-    t_sample *out = (t_sample *)(w[2]);
-    t_lorctl *c = (t_lorctl *)(w[3]);
-    int n = (t_int)(w[4]);
+    //t_sample *in = (t_sample *)(w[1]);
+    t_sample *out0 = (t_sample *)(w[1]);
+    t_sample *out1 = (t_sample *)(w[2]);
+    t_sample *out2 = (t_sample *)(w[3]);
+    t_lorctl *c = (t_lorctl *)(w[4]);
+    int n = (t_int)(w[5]);
     int i;
     t_sample xlast = c->c_x1;
     t_sample xprev = c->c_x2;
@@ -59,98 +54,62 @@ static t_int *siglor_perform(t_int *w)
     t_sample yprev = c->c_y2;
     t_sample zlast = c->c_z1;
     t_sample zprev = c->c_z2;
-    t_sample last = c->c_d1;
-    t_sample prev = c->c_d2;
     t_sample ia = c->c_ia;
     t_sample ib = c->c_ib;
     t_sample ic = c->c_ic;
     t_sample ih = c->c_ih;
-    t_sample min = c->min;
-    t_sample max = c->max;
-    t_sample nmax, nmin, range;
-    //t_sample ff3 = c->c_ff3;
     for (i = 0; i < n; i++)
     {
-        t_sample output;
-        t_sample outt = *out;
+
         xprev = xlast + ih * ia * (ylast - xlast);
         yprev = ylast + ih * (xlast * (ib - zlast) - ylast);
         zprev = zlast + ih * (xlast * ylast - ic * zlast);
-        prev = sqrt((xlast * xlast) + (ylast * ylast)  + (zlast * zlast));
         
-        output =  *in++ + ia * last + ib * prev;
-        if (PD_BIGORSMALL(output))
-            output = 0;
-        outt = (((ih * output + ic * last + prev) / range) * 2. + 1. )* 0.5;
-        xprev = xlast;
-        yprev = ylast;
-        zprev = zlast;
-        prev = last;
-        last = output;	
-        nmax = (outt > max) ? outt : max;
-        nmin = (outt < min) ? outt : min;
-        range = fabsf(min) + max;
-        outt++;
+        *out0++ = xprev;
+        *out1++ = yprev;
+        *out2++ = zprev;
+        
+        xlast = xprev;
+        ylast = yprev;
+        zlast = zprev;
     }
-    c->c_d1 = last;
-    c->c_d2 = prev;
     c->c_x1 = xlast;
     c->c_x2 = xprev;
     c->c_y1 = ylast;
     c->c_y2 = yprev;
     c->c_z1 = zlast;
     c->c_z2 = zprev;
-    c->min = nmin;
-    c->max = nmax;
-    return (w+5);
+    return (w+6);
 }
 
 static void siglor_list(t_siglor *x, t_symbol *s, int argc, t_atom *argv)
 {
-    t_float ia = atom_getfloatarg(0, argc, argv);
-    t_float ib = atom_getfloatarg(1, argc, argv);
-    t_float ic = atom_getfloatarg(2, argc, argv);
-    t_float ih = atom_getfloatarg(3, argc, argv);
-   // t_float discriminant = fb1 * fb1 + 4 * fb2;
+    c->c_ia = atom_getfloatarg(0, argc, argv);
+    c->c_ib = atom_getfloatarg(1, argc, argv);
+    c->c_ic = atom_getfloatarg(2, argc, argv);
+    c->c_ih = atom_getfloatarg(3, argc, argv);
     t_lorctl *c = x->x_ctl;
-    //if (discriminant < 0) /* imaginary roots -- resonant filter */
-    //{
-            /* they're conjugates so we just check that the product
-            is less than one */
-        //if (fb2 >= -1.0f) goto stable;
-    //}
-    //else    /* real roots */
-    //{
-            /* check that the parabola 1 - fb1 x - fb2 x^2 has a
-                vertex between -1 and 1, and that it's nonnegative
-                at both ends, which implies both roots are in [1-,1]. */
-        //if (fb1 <= 2.0f && fb1 >= -2.0f &&
-            //1.0f - fb1 -fb2 >= 0 && 1.0f + fb1 - fb2 >= 0)
-                //goto stable;
-    //}
-        /* if unstable, just bash to zero */
-    //fb1 = fb2 = ff1 = ff2 = ff3 = 0;
-//stable:
-    c->c_ia = ia;
-    c->c_ib = ib;
-    c->c_ic = ic;
-    c->c_ih = ih;
-    //c->c_ff3 = ff3;
 }
 
 static void siglor_set(t_siglor *x, t_symbol *s, int argc, t_atom *argv)
 {
     t_lorctl *c = x->x_ctl;
-    c->c_d1 = atom_getfloatarg(0, argc, argv);
-    c->c_d2 = atom_getfloatarg(1, argc, argv);
-    c->min = 0;
-    c->max = 10000000000;
+    c->c_ia = 0;
+    c->c_ib = 0;
+    c->c_ic = 0;
+    c->c_ih = 0;
+    c->c_x1 = 0;
+    c->c_x2 = 0;
+    c->c_y1 = 0;
+    c->c_y2 = 0;
+    c->c_z1 = 0;
+    c->c_z2 = 0;
 }
 
 static void siglor_dsp(t_siglor *x, t_signal **sp)
 {
-    dsp_add(siglor_perform, 4,
-        sp[0]->s_vec, sp[1]->s_vec,
+    dsp_add(siglor_perform, 5,
+        sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec,
             x->x_ctl, sp[0]->s_n);
 
 }
