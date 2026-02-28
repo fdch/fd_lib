92e685124b883c5e49d5c13ce679e86929b57647
fdch <camarafede@gmail.com>
2026-02-28 10:48:17 +0100

    refactor connect to take connect/disconnect messages instead

--- src/connect.c
+++ src/connect.c
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Copyright 2017-2020 Fede Camara Halac - ffddcchh
 
@@ -10,111 +10,68 @@ fd_lib is distributed in the hope that it will be useful, but WITHOUT ANY WARRAN
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-
 #include "fdLib.h"
-#include "m_imp.h"
-
-typedef struct connect {
-	t_object x_obj;
-	t_symbol *x_who;
-	t_canvas *canvas;
-	t_canvas *x_parent;
-	t_object *x_object;
-} t_connect;
-
-int setcanvassym_connect(t_connect *x, t_symbol *s);
-int glist_getindex(t_glist *x, t_gobj *y);
-
-   /* get the index of a gobj in a glist.  If y is zero, return the
-       total number of objects. */
-int glist_getindex(t_glist *x, t_gobj *y)
-{
-    t_gobj *y2;
-    int indx;
-
-    for (y2 = x->gl_list, indx = 0; y2 && y2 != y; y2 = y2->g_next)
-        indx++;
-    return (indx);
-}
-
-int setcanvassym_connect(t_connect *x, t_symbol *s)
-{
-	x->x_who = s;
-	x->canvas = glist_getcanvas(((t_canvas *)pd_findbyclass(x->x_who, canvas_class)));
-	
-	if (x->canvas) {
-		x->x_object = pd_checkobject((t_pd*)x->canvas);
-		x->x_parent = x->canvas->gl_owner;
-		return 1;
-	} else {
-		bug("setcanvassym_connect");
-		return 0;
-	}
-}
-
-static void connect_doit(t_connect *x, t_int f)
-{
-	t_gobj *y;
-	int ninlets = 0, fro = 0, in = 0, out = 0;
-	int to = 1;
-	int connid = 0;
-	//ninlets = obj_ninlets(x->x_object);
-	
-	
-	for (y = x->canvas->gl_list; y; y = y->g_next)
-	{
-		t_object*obj=(t_object*)y;
-		connid = glist_getindex(x->canvas, (t_gobj*)obj);
-	}
-	if (!connid)
-		post("Found no objects in %s", x->x_who->s_name);
-	else
-		for (to = 0, fro = 1; to < connid; fro++, to++) 
-			if (f == 1)
-				canvas_connect(x->canvas, to, out, fro, in);
-			else
-				canvas_disconnect(x->canvas, to, out, fro, in);
-
-}
-
-static void connect_bang(t_connect *x)
-{
-	if (!x->x_who) {
-		post("No canvas to point to...");
-		return;
-	} else
-		connect_doit(x, 1);
-}
-
-static void connect_symbol(t_connect *x, t_symbol *s)
-{
-	setcanvassym_connect(x,s);
-	connect_bang(x);
-}
-
-static void connect_disconnect(t_connect *x, t_symbol *s)
-{
-	setcanvassym_connect(x,s);
-	if (!x->x_who) {
-		post("No canvas to point to...");
-		return;
-	} else
-		connect_doit(x, 0);
-}
 
 static t_class *connect_class;
 
+typedef struct connect
+{
+  t_object x_obj;
+  t_symbol *x_tgtsym;
+  t_canvas *x_tgtcnv;
+} t_connect;
+
+int connect_settargetcanvas(t_connect *x, t_symbol *s)
+{
+  if (!s)
+    return 0;
+  t_canvas *tgt_canvas = (t_canvas *)pd_findbyclass(s, canvas_class);
+  if (!tgt_canvas)
+    return 0;
+  x->x_tgtcnv = glist_getcanvas(tgt_canvas);
+  if (!x->x_tgtcnv)
+  {
+    bug("connect: connect_settargetcanvas failed to get canvas.");
+    return 0;
+  }
+  x->x_tgtsym = s;
+  return 1;
+}
+
+static void connect_connect(t_connect *x, t_floatarg fwhoout, t_floatarg foutno,
+                            t_floatarg fwhoin, t_floatarg finno)
+{
+  if(!connect_settargetcanvas(x, x->x_tgtsym))
+    return;
+  if(!glist_isvisible(x->x_tgtcnv))
+    canvas_vis(x->x_tgtcnv, 1.0);
+  canvas_connect(x->x_tgtcnv, fwhoout, foutno, fwhoin, finno);
+}
+
+static void connect_disconnect(t_connect *x, t_floatarg index1, t_floatarg outno,
+                            t_floatarg index2, t_floatarg inno)
+{
+  if(!connect_settargetcanvas(x, x->x_tgtsym))
+    return;
+  if(!glist_isvisible(x->x_tgtcnv))
+    canvas_vis(x->x_tgtcnv, 1.0);
+  canvas_disconnect(x->x_tgtcnv, index1, outno, index2, inno);
+}
+
 static void *connect_new(t_symbol *s)
 {
-	t_connect *x = (t_connect *)pd_new(connect_class);
-	if (*s->s_name)
-		setcanvassym_connect(x,s);
-	return (x);
+  t_connect *x = (t_connect *)pd_new(connect_class);
+  symbolinlet_new(&x->x_obj, &x->x_tgtsym);
+  if (!connect_settargetcanvas(x, s))
+      pd_error(x, "%s: could not set canvas.", s->s_name);
+  return (x);
 }
 
 void connect_setup() {
-	connect_class = class_new(gensym("connect"), (t_newmethod)connect_new, 0, sizeof(t_connect), CLASS_DEFAULT, A_DEFSYM, 0);
-	class_addbang(connect_class, connect_bang);
-	class_addsymbol(connect_class, connect_symbol);
-	class_addmethod(connect_class, (t_method)connect_disconnect, gensym("disconnect"), A_SYMBOL, 0);
+  connect_class = class_new(gensym("connect"), (t_newmethod)connect_new,0,
+                            sizeof(t_connect), CLASS_DEFAULT, A_DEFSYM, 0);
+  class_addmethod(connect_class, (t_method)connect_disconnect,
+                  gensym("disconnect"), A_FLOAT, A_FLOAT, A_FLOAT, A_FLOAT, A_NULL);
+  class_addmethod(connect_class, (t_method)connect_connect,
+                  gensym("connect"), A_FLOAT, A_FLOAT, A_FLOAT, A_FLOAT, A_NULL);
 }
