8f6e04b9f8cfc775a05a7055218456544ee10ef9
fdch <camarafede@gmail.com>
2026-03-09 17:06:13 +0100

    minor formatting

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Copyright 2017-2020 Fede Camara Halac - ffddcchh
 
@@ -10,7 +10,6 @@ fd_lib is distributed in the hope that it will be useful, but WITHOUT ANY WARRAN
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-
 #include "fdLib.h"
 
 /*
@@ -33,35 +32,32 @@ typedef struct _mandelbrot
 } t_mandelbrot;
 
 
-static void *mandelbrot_new(t_floatarg f)
+static void *mandelbrot_new()
 {
     t_mandelbrot *x = (t_mandelbrot *)pd_new(mandelbrot_class);
-    x->k=f;
-    if (! x->k) {x->k=100;}
     floatinlet_new(&x->x_obj, &x->ci);
     floatinlet_new(&x->x_obj, &x->k);
     outlet_new(&x->x_obj,0);
+    x->k = 100;
     return (x);
 }
 
-
-
 static void mandelbrot_bang(t_mandelbrot *x)
-{
-   outlet_float(x->x_obj.ob_outlet,x->result); 
-}
+{ outlet_float(x->x_obj.ob_outlet, x->result); }
 
 static void mandelbrot_float(t_mandelbrot *x, t_float f)
-{ 
+{
   int i = -2;
   t_float zr = 0;
   t_float zi = 0;
   t_float z2r = 0;
   t_float z2i = 0;
   x->cr = f;
-  if ((x->cr < 0 ) && ( x->cr > -0.5) && (x->ci * x->ci < 0.25)){
+
+  if ((x->cr < 0 ) && ( x->cr > -0.5) && (x->ci * x->ci < 0.25))
     x->result = x->k;
-  }else{
+  else
+  {
     while (((z2r + z2i) < 4 ) && (++i < x->k )) {
       z2r = zr * zr;
       z2i = zi * zi;
@@ -69,17 +65,14 @@ static void mandelbrot_float(t_mandelbrot *x, t_float f)
       zr = z2r - z2i + x->cr;
     }
     x->result = i;
-  }  
-  outlet_float(x->x_obj.ob_outlet,x->result);
+  }
+  outlet_float(x->x_obj.ob_outlet, x->result);
 }
-
-
 
 void mandelbrot_setup(void)
 {
     mandelbrot_class = class_new(gensym("mandelbrot"), (t_newmethod)mandelbrot_new, 0,
-    	sizeof(t_mandelbrot), 0, A_DEFFLOAT, 0);
+                                 sizeof(t_mandelbrot), 0, A_NULL);
     class_addbang(mandelbrot_class, mandelbrot_bang);
-    class_addfloat(mandelbrot_class, mandelbrot_float);        
-/*    class_addfloat(mandelbrot_class; mandelbrot_float);*/
+    class_addfloat(mandelbrot_class, mandelbrot_float);
 }
