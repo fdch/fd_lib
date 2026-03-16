4a72b27f94398d611bccd6844c98d9acdaad32d6
fdch <camarafede@gmail.com>
2026-03-16 10:47:52 +0100

    udpate root

--- src/root.c
+++ src/root.c
@@ -16,16 +16,15 @@ should have received a copy of the GNU General Public License along with this
 program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-
 #include "fdLib.h"
+
+static t_class *root_class;
 
 typedef struct root
 {
     t_object x_ob;
     t_glist *x_glist;
 } t_root;
-
-static t_class *root_class;
 
 static void root_bang(t_root *x)
 {
@@ -36,20 +35,20 @@ static void root_bang(t_root *x)
     else
         snprintf(buf, MAXPDSTRING - 1, ".x%lx", (t_int)x->x_glist->gl_name);
     buf[MAXPDSTRING - 1] = 0;
-    outlet_symbol(x->x_ob.ob_outlet, gensym(buf));
+    outlet_symbol(x->x_ob.te_outlet, gensym(buf));
 }
 
 static void *root_new()
 {
     t_root *x = (t_root *)pd_new(root_class);
     x->x_glist = canvas_getcurrent();
-    outlet_new(&x->x_ob, &s_symbol);
+    outlet_new(&x->x_ob, gensym("symbol"));
     return (void *)x;
 }
 
 void root_setup(void)
 {
     root_class = class_new(gensym("root"), (t_newmethod)root_new, 0,
-                           sizeof(t_root), 0, 0);
+                           sizeof(t_root), 0, A_NULL);
     class_addbang(root_class, root_bang);
 }
