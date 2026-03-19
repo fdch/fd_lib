2e6cb827f9acb9a077e72e156d217f9cf2673eee
fdch <camarafede@gmail.com>
2026-03-19 20:14:54 +0100

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,10 +5,20 @@
 #
 VERSION="v$(grep version README.txt | cut -f 2 -d: | sed 's/ //')"
 TAGS=$(git tag -l)
+FORCE=0
+if [[ "$1" == "-f" ]]; then
+	FORCE=1
+fi
 for tag in ${TAGS}; do
 	if [[ $VERSION == $tag ]]; then
 		echo "Version $tag already exists. Increment version."
-		exit 1
+		if [[ $FORCE == 1 ]]; then
+			echo "Attempting to force release."
+			git push --delete origin $VERSION
+			git tag -d $VERSION
+		else
+			exit 1
+		fi
 	fi
 done
 echo "Releasing tag $VERSION"
