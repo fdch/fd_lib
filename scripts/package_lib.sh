985ac8022e52624199965bde2972e5e8016f6425
fdch <camarafede@gmail.com>
2026-03-19 17:13:14 +0100

    fix gh actions

--- scripts/package_lib.sh
+++ scripts/package_lib.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #
 # package the library
+# instead of using make install from pdlibbuilder
+# because it does not put the sources in it
 #
 #
-mkdir -p fd_lib/sources/pd-lib-builder
-cp fd_lib.* {.,abstractions,help}/*.pd fd_lib/
-cp -r examples doc data img scripts test fd_lib/
-cp Makefile *.{sh,txt} fd_lib/sources/
-cp src/*.{h,c} fd_lib/sources/
-cp pd-lib-builder/Makefile.pdlibbuilder fd_lib/sources/pd-lib-builder/
+target_dir="fd_lib"
+if [[ $1 ]]; then
+	target_dir="$1"
+fi
+mkdir -p "$target_dir/sources/pd-lib-builder"
+cp fd_lib.* {.,abstractions,help}/*.pd "$target_dir"
+cp -r examples doc data img scripts test "$target_dir"
+cp Makefile *.{sh,txt} "$target_dir/sources/"
+cp src/*.{h,c} "$target_dir/sources/"
+cp pd-lib-builder/Makefile.pdlibbuilder "$target_dir/sources/pd-lib-builder/"
